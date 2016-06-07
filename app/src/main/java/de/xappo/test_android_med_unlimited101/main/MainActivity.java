2e2af3409a58396a93fb3f49ca086815b42e372c
Michael Knoppik <Michael.Knoppik@mk-xappo.de>
2016-06-07 11:26:11 +0200

    Changed main root layout to LinearLayout

--- app/src/main/java/de/xappo/test_android_med_unlimited101/main/MainActivity.java
+++ app/src/main/java/de/xappo/test_android_med_unlimited101/main/MainActivity.java
@@ -1,7 +1,9 @@
-package de.xappo.test_android_med_unlimited101;
+package de.xappo.test_android_med_unlimited101.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import de.xappo.test_android_med_unlimited101.R;
 
 public class MainActivity extends AppCompatActivity {
 
